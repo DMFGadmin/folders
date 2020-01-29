32cb5bcb4d2c3e281450e9d59b44ee4ea7a2bf8d
joel <joelcote68@gmail.com>
2020-01-29 11:09:33 -0600

    adding terraform example folder

--- modules/folders/main.tf
+++ modules/folders/main.tf
@@ -14,3 +14,9 @@ resource "google_folder" "afrl-testing" {
   display_name = "AFRL-Testing"
   parent       = "organizations/${var.organization_id}"
 }
+
+# Terraform example folder.
+resource "google_folder" "afrl-terraform-example" {
+  display_name = "AFRL-terraform-example"
+  parent       = "organizations/${var.organization_id}"
+}git a
